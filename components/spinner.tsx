aa76ddbbb4c0eee01830539ef92311c4c4362aba
김용순 <dydtns9815@gmail.com>
2025-01-14 16:33:41 +0900

    Refactor Spinner component and move CSS to styles directory

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,10 +1,5 @@
-// import spinner.css in the same folder
-import "./spinner.css";
+import "../styles/spinner.css";
 
 export default function Spinner() {
-  return (
-    <div className="spinner-container">
-      <div className="spinner"></div>
-    </div>
-  );
+  return <div className="spinner"></div>;
 }
