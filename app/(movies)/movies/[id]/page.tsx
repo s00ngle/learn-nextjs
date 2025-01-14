68a5cff3de85053da630bb2e02493d057d0481b3
김용순 <dydtns9815@gmail.com>
2025-01-14 18:19:03 +0900

    Refactor import paths to use absolute imports in MovieDetail component

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,16 +1,14 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
-import MovieVideos from "../../../../components/movie-videos";
-import Spinner from "../../../../components/spinner";
+import MovieInfo from "@/components/movie-info";
+import MovieVideos from "@/components/movie-videos";
+import Spinner from "@/components/spinner";
 
-// async로 컴포넌트 정의
 export default async function MovieDetail({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = await params; // 여기에서 비동기적으로 처리
-
+  const { id } = await params;
   return (
     <div>
       <h1>Movie Detail Page</h1>
